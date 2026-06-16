52cdad19f2415d95f76c434717415134d92cc123
Lorenzo Ferrara <lo.ferrara@reply.it>
2026-06-16 18:20:01 +0200

    fix signin page

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Admin from "./pages/Admin";
 import Polls from "./pages/Polls";
 import Poll from "./pages/Poll";
 import Results from "./pages/Results";
+import SignIn from "./pages/SignIn";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sign-in" element={<App />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<Admin />} />
           <Route path="/polls" element={<Polls />} />
